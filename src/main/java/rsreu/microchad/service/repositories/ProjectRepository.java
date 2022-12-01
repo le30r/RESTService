9c1841390cf5d4e4d37f383a05192581e363e759
Makarov Oleg <bogdanolegom@gmail.com>
2022-12-01 15:44:27 +0300

    Adding half-repositories

---
+++ src/main/java/rsreu/microchad/service/repositories/ProjectRepository.java
@@ -0,0 +1,8 @@
+package rsreu.microchad.service.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import rsreu.microchad.service.entities.Project;
+
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+}
---
+++ src/main/java/rsreu/microchad/service/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package rsreu.microchad.service.repositories;
+
+
+import org.springframework.data.repository.CrudRepository;
+import rsreu.microchad.service.entities.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
+}
