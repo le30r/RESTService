3a103fe56331d225ff736d4a85e087b40e5b1f6d
Ivan Marinin <marivann2001@gmail.com>
2022-12-01 15:48:34 +0300

    added versioning for swagger

--- src/main/java/rsreu/microchad/service/config/SwaggerConfiguration.java
+++ src/main/java/rsreu/microchad/service/config/SwaggerConfiguration.java
@@ -1,19 +1,25 @@
 package rsreu.microchad.service.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
 public class SwaggerConfiguration {
     private ApiInfo apiInfo() {
-        return new ApiInfo("MyApp Rest APIs",
-                "APIs for MyApp.",
+        return new ApiInfo("HR App",
+                "API для управления персоналом.",
                 "1.0",
                 "Terms of service",
                 new Contact("test", "www.org.com", "test@emaildomain.com"),
@@ -23,13 +29,13 @@ public class SwaggerConfiguration {
     }
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.OAS_30)
-                .apiInfo(apiInfo())
+    public Docket apiV1() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("api-v1.0")
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
+                .apis(RequestHandlerSelectors.basePackage("rsreu.microchad.service.contollers.v1"))
+                .build()
+                .apiInfo(apiInfo());
     }
-
 }
+
