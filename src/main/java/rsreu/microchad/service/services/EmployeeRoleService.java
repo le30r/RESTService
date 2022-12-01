8df24aaef6e14ec942acf82167293b90f3df00d4
Artem Melihov <argon1910@mail.ru>
2022-12-01 15:38:29 +0300

    Adding services

---
+++ src/main/java/rsreu/microchad/service/services/EmployeeRoleService.java
@@ -0,0 +1,12 @@
+package rsreu.microchad.service.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeRoleService {
+
+
+
+
+}
---
+++ src/main/java/rsreu/microchad/service/services/EmployeeServices.java
@@ -0,0 +1,10 @@
+package rsreu.microchad.service.services;
+
+import org.springframework.stereotype.Service;
+import rsreu.microchad.service.entities.Employee;
+
+@Service
+public class EmployeeServices {
+
+
+}
