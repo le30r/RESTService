80c78401057046b6d8a4ec83ebc5f7ad0a4eb6c1
Makarov Oleg <bogdanolegom@gmail.com>
2022-12-02 14:47:00 +0300

    Adding RoleController

--- src/main/java/rsreu/microchad/service/contollers/v1/RoleController.java
+++ src/main/java/rsreu/microchad/service/contollers/v1/RoleController.java
@@ -2,37 +2,74 @@ package rsreu.microchad.service.contollers.v1;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import rsreu.microchad.service.dto.ProjectDto;
+import rsreu.microchad.service.dto.EmployeeDto;
 import rsreu.microchad.service.dto.RoleDto;
+import rsreu.microchad.service.entities.Role;
+import rsreu.microchad.service.services.EmployeeService;
+import rsreu.microchad.service.services.RoleService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Objects;
 
 @RestController
 @Api
 @RequestMapping("/api/v1/role")
 public class RoleController {
 
+    RoleService roleService;
+
+    public RoleController(@Autowired RoleService roleService) {
+        this.roleService = roleService;
+    }
+
     @ApiOperation(value = "Получить информацию о роли")
     @GetMapping(value = "/id={id}")
-    public ResponseEntity get(Long id) {
-        return ResponseEntity.ok("ok");
+    public ResponseEntity get(@PathVariable Long id) {
+        var result = roleService.findById(id);
+        try {
+            return new ResponseEntity<>(roleService.findById(id), HttpStatus.ACCEPTED);
+        }
+        catch (NoSuchElementException exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @ApiOperation(value = "Обновить информацию о роли")
     @PutMapping(value = "/id={id}")
-    public ResponseEntity update(Long id, @RequestBody RoleDto roleDto) {
-        return ResponseEntity.ok("ok");
+    public ResponseEntity update(@PathVariable Long id, @RequestBody RoleDto roleDto) {
+        try {
+            return new ResponseEntity<>(roleService.update(roleDto), HttpStatus.OK);
+        }
+        catch (NoSuchElementException exception) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
-    @ApiOperation(value = "Добавить информацию о роли")
+    @ApiOperation(value = "Добавить роль")
     @PostMapping
     public ResponseEntity add(@RequestBody RoleDto roleDto) {
-        return ResponseEntity.ok("ok");
+        if(roleService.save(roleDto)) {
+            return new ResponseEntity(HttpStatus.CREATED);
+        } else {
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        }
     }
 
     @ApiOperation(value = "Удалить информацию о роли")
     @DeleteMapping(value = "/id={id}")
-    public ResponseEntity remove(Long id) {
-        return ResponseEntity.ok("ok");
+    public ResponseEntity remove(@PathVariable Long id) {
+        roleService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "/")
+    @ApiOperation(value = "Получение информации о всех ролях")
+    public ResponseEntity<List<RoleDto>> getAll() {
+        return new ResponseEntity<>(roleService.findAll(), HttpStatus.OK);
     }
 }
